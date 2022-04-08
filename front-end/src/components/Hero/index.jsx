4c19ca3fc1670ff4f4d5bdbae782dbc27637fcb3
Rafael Sampaio <rafael.sampa1989@gmail.com>
2022-04-08 09:46:41 -0300

    feat/cria-componente-hero

---
+++ front-end/src/components/Hero/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css"
+
+export function Hero() {
+
+  return(
+    <section className="hero">
+      <div className="content">
+        <h1>Lorem, ipsum dolor sit amet?</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur corporis quasi nam labore eius! Repellat inventore, voluptas magnam rerum aliquam saepe nemo distinctio laboriosam laudantium deleniti, consequatur natus quibusdam.</p>
+        <button>Busque seu mentor</button>
+      </div>
+    </section>
+  );
+};
