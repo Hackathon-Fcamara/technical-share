953ffe41fdc01dd9d3c20b698258c2d496806143
Brian Rocha <rochabrian120@gmail.com>
2022-04-08 10:27:25 -0300

    feat: cria esqueleto do footer
    todo o esqueleto do footer criado

---
+++ front-end/src/components/Ratings/Footer/index.js
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <section className={footer}>
+      <figure className={footer__imgWrapper}>
+        <img className={footer__img} src="" alt="" />
+      </figure>
+      <h2 className={footer__title}></h2>
+      <div className={footer__iconWrapper}>
+        <span className={footer__icon}></span>
+        <span className={footer__icon}></span>
+        <span className={footer__icon}></span>
+        <span className={footer__icon}></span>
+      </div>
+    </section>
+  );
+};
