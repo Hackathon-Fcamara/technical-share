9724292bdf83b146314cb466cdfb98165ef5a78a
Rafael Sampaio <rafael.sampa1989@gmail.com>
2022-04-11 19:43:28 -0300

    refactor: ajusta responsividade signInForm

--- front-end/src/components/SignInForm/index.js
+++ front-end/src/components/SignInForm/index.js
@@ -5,6 +5,7 @@ const {
   signInForm,
   signInForm__container,
   signInForm__containerTitle,
+  signInForm__containerInput,
   signInForm__containerButton,
 } = styles;
 
@@ -13,12 +14,16 @@ export function SignInForm() {
     <div className={signInForm}>
       <div className={signInForm__container}>
         <h1 className={signInForm__containerTitle}>Já é cadastrado?</h1>
-        <label>
-          <input type="email" />
-        </label>
-        <label>
-          <input type="password" />
-        </label>
+        <div className={signInForm__containerInput}>
+          <label htmlFor="email">
+            <span>E-mail</span>
+            <input type="email" name="email" id="signInEmail" />
+          </label>
+          <label htmlFor="password">
+          <span>Senha</span>
+            <input type="password" name="password" id="signInPassword" />
+          </label>
+        </div>
         <button type="button" className={signInForm__containerButton}>
           Logar
         </button>
