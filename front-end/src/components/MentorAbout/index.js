bc483876a6f3f0695cecd9a42aec365018d7a297
Brian Rocha <rochabrian120@gmail.com>
2022-04-11 19:10:23 -0300

    feat: cria componentes modalLogin e mentorAbout
    criação dos componentes modalLogin e mentorAbout junto com seus arquivos
    de style vazios

---
+++ front-end/src/components/MentorAbout/index.js
@@ -0,0 +1,21 @@
+export function mentorAbout() {
+  return (
+    <section className={about}>
+      <div className={about__infoWrapper}>
+        <button className={about__button}>
+          <p className={about__info}></p>
+        </button>
+        <div className={about__depoimentsWrapper}>
+          <button className={about__secondButton}>
+            <p className={about__depoiments}></p>
+          </button>
+          <div className={about__statisticsWrapper}>
+            <button className={about__thirdButton}>
+              <p className={about__statistics}></p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
