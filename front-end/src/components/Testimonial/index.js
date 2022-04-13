cb018519a9d8ccec41cb1439cdd71f967ca01809
leotx <leotx@pop-os.localdomain>
2022-04-13 10:32:33 -0300

    feat: cria os estilos do componente Testimonial para telas pequenas

--- front-end/src/components/Testimonial/index.js
+++ front-end/src/components/Testimonial/index.js
@@ -1,25 +1,48 @@
-export const Testmonial = () => {
+import styles from "./styles.module.css";
+
+import imageGuy from "../../assets/images/image-guy.jpg";
+
+const {
+  testimonial__container,
+  testimonial__firstTitle,
+  testimonial__cardList,
+  testimonial__card,
+  testimonial__img,
+  testimonial__textWrapper,
+  testimonial__secondTitle,
+  testimonial__role,
+  testimonial__aboutTitle,
+  testimonial__aboutText,
+  testimonial__author,
+} = styles;
+
+export const Testimonial = () => {
   return (
-    <section className={testimonial}>
-      <h2 className={testimonial__firstTitle}></h2>
-      <div className={testimonial__wrapper}>
-        <figure className={testimonial__imgWrapper}>
-          <img className={testimonial__img} src="" alt="" />
-        </figure>
-        <div className={testimonial__textWrapper}>
-          <h3 className={testimonial__secondTitle}></h3>
-          <span className={testimonial__role}></span>
-          <div className={testimonial__starsWrapper}>
-            <span className={testimonial__star}></span>
-            <span className={testimonial__star}></span>
-            <span className={testimonial__star}></span>
-            <span className={testimonial__star}></span>
-            <span className={testimonial__star}></span>
-          </div>
-          <p className={testimonial__aboutTitle}></p>
-          <p className={testimonial__aboutText}></p>
-          <span className={testimonial__author}></span>
-        </div>
+    <section>
+      <div className={testimonial__container}>
+        <h2 className={testimonial__firstTitle}>Depoimentos</h2>
+        <ul className={testimonial__cardList}>
+          <li className={testimonial__card}>
+            <figure>
+              <img
+                className={testimonial__img}
+                src={imageGuy}
+                alt="Foto do mentor"
+              />
+            </figure>
+            <div className={testimonial__textWrapper}>
+              <h3 className={testimonial__secondTitle}>Luciano Tavares</h3>
+              <span className={testimonial__role}>Dev Front-End Sênior</span>
+              <p className={testimonial__aboutTitle}>O que falam sobre</p>
+              <p className={testimonial__aboutText}>
+                Com a plataforma Technical Share pude encontrar exatamente o que
+                eu precisava do meu mentor. E através de uma mentoria assertiva
+                consegui organizar melhor meus estudos.{" "}
+              </p>
+              <span className={testimonial__author}>Depoimento de Matheus</span>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
