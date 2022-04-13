9ed8901f35d813bf14301fd8c3bf6ddf992fe588
leotx <leotx@pop-os.localdomain>
2022-04-13 16:38:28 -0300

    refactor: refatora a estrutura do componente SignUp
    - Altera o nome do componente SignUpForm para SignUp
    - Adiciona novos estilos para versao mobile do componente
    - Adiciona novos regras de estilos globais

--- front-end/src/pages/PageRegister/index.js
+++ front-end/src/pages/PageRegister/index.js
@@ -1,14 +1,14 @@
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { SignInForm } from "../../components/SignInForm";
-import { SignUpForm } from "../../components/SignUpForm";
+import { SignUp } from "../../components/SignUpForm";
 
 export const PageRegister = () => {
   return (
     <>
       <Header />
       <SignInForm />
-      <SignUpForm />
+      <SignUp />
       <Footer />
     </>
   );
