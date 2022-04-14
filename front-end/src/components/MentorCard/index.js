e480643a3c3f17efaf247422006bbcdaa2d2cffc
Rafael Sampaio <rafael.sampa1989@gmail.com>
2022-04-14 15:37:52 -0300

    feat: popula componente mentorCard

--- front-end/src/components/MentorCard/index.js
+++ front-end/src/components/MentorCard/index.js
@@ -1,5 +1,7 @@
 import styles from "./styles.module.css";
 import illustration from "../../assets/images/image-foto-illustration.png";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 const {
   mentorCard,
@@ -10,27 +12,44 @@ const {
 } = styles;
 
 export const MentorCard = () => {
+
+  const [ mentorInfo, setMentorInfo ] = useState([]);
+
+  useEffect(() => {
+    api.get("usuarios/all").then(({data}) => {
+      setMentorInfo(data)
+    })
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  console.log(mentorInfo)
+  
   return (
-    <div className={mentorCard}>
-      <figure className="mentorCard__imgWrapper">
-        <img
-          src={illustration}
-          alt="Foto do mentor"
-          className={mentorCard__img}
-        />
-      </figure>
-      <div className={mentorCard__textWrapper}>
-        <h3 className="mentorCard__name">Placeholder name</h3>
-        <span className="mentorCard__role">Placeholder skill</span>
-        <ul className={mentorCard__skillList}>
-          <li className="mentorCard__skill">CSS</li>
-          <li className="mentorCard__skill">React</li>
-          <li className="mentorCard__skill">Angular</li>
-        </ul>
-        <a href="" className={mentorCard__scheduleLink}>
-          Visitar perfil
-        </a>
-      </div>
-    </div>
+    <>
+      {mentorInfo?.map((r) => (
+        <div key={r.id} className={mentorCard}>
+          <figure className="mentorCard__imgWrapper">
+            <img
+              src={illustration}
+              alt="Foto do mentor"
+              className={mentorCard__img}
+            />
+          </figure>
+          <div className={mentorCard__textWrapper}>
+            <h3 className="mentorCard__name">{r.nome}</h3>
+            <span className="mentorCard__role">{`${r.cargo} ${r.nivel}`}</span>
+            <ul className={mentorCard__skillList}>
+              {r.skils.map((skils) => (
+                <li key={skils.id} >{skils.skil}</li>
+              ))}
+            </ul>
+            <a href="" className={mentorCard__scheduleLink}>
+              Visitar perfil
+            </a>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
